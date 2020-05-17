55721be95e0dc4f72d087ce30e8d590359cd76ce
danylochi <65088681+danylochi@users.noreply.github.com>
2020-05-16 22:27:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var lista = ["maça","pera","laranja"];
+//lista.push ("uva");
+//lista.pop ("laranja");
+console.log(lista,length);
+
+//var nome = "Dayane Domingues";
+//var idade = 30;
+//var idade2= 10;
+//var frase= "Japão é o melhor do mundo";
+//alert(nome + "tem" + idade + anos);
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"))
